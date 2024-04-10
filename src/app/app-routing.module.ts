0b872375446d60222721517aafbb55f117db022c
Desislava Georgieva <desislava.georgieva@bbraun.com>
2024-04-10 10:54:36 +0300

    MicroFE with Angular 16

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {NewPackageComponent} from "./new-package/new-package.component";
 
-const routes: Routes = [];
+export const APP_ROUTES: Routes = [
+  {
+    path: '',
+    redirectTo: 'packages',
+    pathMatch: 'full'
+  },
+  {
+    path: 'new-package',
+    component: NewPackageComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(APP_ROUTES)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
